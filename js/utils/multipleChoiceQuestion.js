cb92dd6af0c3bf7e3c4d33ea0851a4fd7f4b5f07
Johan Henkens <johan@henkens.com>
2013-03-06 19:56:43 -0800

    Added selectNumAnsweres to mcQ

--- js/utils/multipleChoiceQuestion.js
+++ js/utils/multipleChoiceQuestion.js
@@ -9,4 +9,10 @@ function MultipleChoiceQuestion(questionText,correctAnswer,incorrectAnswers){
             return this.incorrectAnswers;
         }
     };
+    this.selectNumAnswers = function(numOfAnswers){
+        var pos = _.random(0,numOfAnswer-1);
+        var arr = this.getIncorrectAnswers(numOfAnswers-1);
+        arr.splice(pos,0,this.correctAnswer);
+        return [arr,pos];
+    }
 };
