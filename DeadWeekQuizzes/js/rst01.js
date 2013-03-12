ed01698f9196eed7d2bd3507ea87a2000430e330
pconrad <pconrad@cs.ucsb.edu>
2013-03-12 11:33:31 -0700

    Dead Week Exercise for RST

---
+++ DeadWeekQuizzes/js/rst01.js
@@ -0,0 +1,46 @@
+// rst01.js   
+
+function generateExercise() {
+    
+    var questions = "";
+    var answers = "";
+
+    exercises = generateReflexiveSymmetricTransitiveQuestions(10);
+    for (var i=0; i<exercises.length; i++ ) {
+	
+	// The question text is now at exercises[i].questionText
+	
+	questions += "<p style='margin-top:2em;'> (" 
+	    + (i+1) 
+	    + ") " 
+	    + exercises[i].questionText + "</p>";
+	
+	var numChoices = 5;
+	
+	numAnswersResult = exercises[i].selectNumAnswers(numChoices);
+	
+	// Now, numAnswersResult[0] is an array of answers
+	// numAnswersResult[1] is the index of the correct answer.
+	
+	theAnswers = numAnswersResult[0];
+	correctAnswerIndex = numAnswersResult[1];
+
+	questions += "<ol style='list-style-type:lower-alpha'>";
+	
+	for (var j=0; j < numChoices; j++) {
+	    questions+="<li>" + theAnswers[j] + "</li>";
+	} // for loop over all answer choices
+	
+	questions += "</ol>";
+	
+	var letters = ["a","b","c","d","e"];
+
+	answers += "<p> (" + (i+1) + ") " + letters[correctAnswerIndex]
+	    + ". " + theAnswers[correctAnswerIndex]
+	    + "</p>";
+	
+	window.document.getElementById("questions").innerHTML = questions; 
+	window.document.getElementById("answers").innerHTML = answers; 
+    } // for loop over all exercises
+    
+}
