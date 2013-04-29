b7880e145a627cdd12d495711c30781706cbcf45
Phill Conrad <pconrad@cs.ucsb.edu>
2013-04-29 13:56:16 -0700

    First version of MakeRandomTests that actually produces a test

--- java/RandomUnitTesting/testRandom.js
+++ java/RandomUnitTesting/testRandom.js
@@ -1,2 +1,4 @@
-test("randomTest",0,function() {
+test("randomTest",1,function() {
+ var r = new Random(new LongBitString(0x123456,0x789ABC));
+equal(1560403464,r.nextInt(),'first call to r.nextInt()');
 });
