b6e76ae3fe2c31b9f297ccf54f38b6d2d60a268d
embarnard <emiliebarnard@gmail.com>
2013-05-23 22:31:02 -0700

    HTML page added for random testing

--- java/RandomUnitTesting/testRandom.js
+++ java/RandomUnitTesting/testRandom.js
@@ -1,4 +1,9 @@
-test("randomTest",1,function() {
+test("testRandom",2,function() {
+
  var r = new Random(new LongBitString(0x123456,0x789ABC));
+ 
 equal(1560403464,r.nextInt(),'first call to r.nextInt()');
+
+equal(1,1,"stub test");
+
 });
