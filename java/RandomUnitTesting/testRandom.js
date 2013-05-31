fe6d7c24383e771a786f1ff2d117b4861c0c28f9
embarnard <emiliebarnard@gmail.com>
2013-05-31 00:01:09 -0700

    Implementation of Next has started
    New issue with times: Java next uses 64 bit numbers in the derivation
    of the next 48 bit number. Need to rethink bits library.

--- java/RandomUnitTesting/testRandom.js
+++ java/RandomUnitTesting/testRandom.js
@@ -1,6 +1,6 @@
-/* for quick java testing: http://rextester.com/QRT10759 */
+/* for quick java testing: http://rextester.com/OMOGXL21081 */
 
-test("testRandom",3,function() {
+test("testRandom",4,function() {
 
 var r = new Random(new LongBitString(0x123456,0x789ABC));
 
@@ -13,5 +13,10 @@ equal(new LongBitString(0x123188,0x947cd1).isEqual(r.currentSeed),true,'testing 
 r.setSeed(new LongBitString(0x123ABC,0x000000));
 
 equal(new LongBitString(0x123F62,0xECE66D).isEqual(r.currentSeed),true,'testing setSeed with seed value 0x123ABC000000'); 
+
+r.next(32);
+
+equal(new LongBitString(0xDA35E8,0x600A74).isEqual(r.currentSeed),true,'testing one step of next (32 bits) with intial seed value 0x123ABC000000'); 
+
 
 });
